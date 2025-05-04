fc1d1298363b43438db3c2477be6497a0961dfe8
Mohsan <mohsan0073@gmail.com>
2025-05-04 13:58:38 +0700

    use async tokio::net

--- src/p2p/network.rs
+++ src/p2p/network.rs
@@ -1,7 +1,8 @@
 use crate::p2p::peer;
 
-use super::peer::{Peer, LocalPeer};
-use std::{collections::HashMap, net::UdpSocket, thread::JoinHandle};
+use super::peer::{LocalPeer, Peer};
+use std::{collections::HashMap, thread::JoinHandle};
+use tokio::net::UdpSocket;
 use uuid::Uuid;
 
 pub struct Server {
@@ -11,32 +12,33 @@ pub struct Server {
 impl Server {
     pub async fn new() -> Self {
         Server {
-            connected_peers: HashMap::default()
+            connected_peers: HashMap::default(),
         }
     }
 
-    async fn listen_to_multicase() -> (){
-        let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    async fn listen_to_multicaste() -> () {
+        let socket = UdpSocket::bind("0.0.0.0:55156").await.unwrap();
         let mut buffer = [0u8; 1024];
         loop {
-            match socket.recv_from(&mut buffer) {
+            println!("Waiting for the client...");
+            match socket.recv_from(&mut buffer).await {
                 Ok((size, source)) => {
                     let peer_info = String::from_utf8_lossy(&buffer);
                     println!("{}", peer_info)
-                },
+                }
                 Err(_) => {
                     print!("Something went wrong")
-                }
+                }             
             }
-        }   
+        }
     }
 
     pub async fn run(&self) {
-
-
         // Start Peer discovery process
-        tokio::spawn(Self::listen_to_multicase()).await;
-        tokio::spawn(LocalPeer::announce("new")).await;
+        let multicast_handle = tokio::spawn(async move { Self::listen_to_multicaste().await });
+        let announce_handle = tokio::spawn(async move { LocalPeer::announce("new").await });
+        tokio::try_join!(multicast_handle, announce_handle).unwrap();
+
         // Start Server
 
         // Start Client
--- src/p2p/peer.rs
+++ src/p2p/peer.rs
@@ -1,6 +1,7 @@
 // use core::marker::Copy;
 use serde::{Deserialize, Serialize};
-use std::net::{IpAddr, Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::net::{IpAddr, Ipv4Addr, SocketAddrV4};
+use tokio::net::UdpSocket;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -9,9 +10,7 @@ pub struct LocalPeer {
     id: Uuid,
 }
 
-pub struct Peer {
-
-}
+pub struct Peer {}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum PeerMessage {
@@ -24,22 +23,31 @@ const PORT: u16 = 54321;
 
 impl LocalPeer {
     pub async fn announce(name: &str) {
+        println!("I am announcing myself..."); 
         let peer = LocalPeer {
             name: name.to_string(),
             id: Uuid::new_v4(),
         };
 
-        let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+        let socket = UdpSocket::bind("0.0.0.0:55155").await.unwrap();
         let multicase_socket = SocketAddrV4::new(Ipv4Addr::new(224, 0, 0, 123), 54321);
-        socket.join_multicast_v4(&MULTICASE_ADD, &Ipv4Addr::UNSPECIFIED);
+        socket.join_multicast_v4(MULTICASE_ADD, Ipv4Addr::UNSPECIFIED).unwrap();
         let mut message = PeerMessage::Connecting;
         let mut buffer = [0u8; 256];
         while let PeerMessage::Connecting = message {
-            socket.send_to(serde_json::to_string(&peer).unwrap().as_bytes(), multicase_socket);
-            let bytes = socket.recv_from(&mut buffer).unwrap();
-
-            if bytes.0 > 0 {
-                message = PeerMessage::Connected;
+            println!("Trying to connect...");
+            socket.send_to(
+                serde_json::to_string(&peer).unwrap().as_bytes(),
+                multicase_socket,
+            ).await.unwrap();
+            println!("Sending to multicast socket...");
+            match socket.try_recv_from(&mut buffer) {
+                Ok((size, result)) => {
+                    message =  PeerMessage::Connected;
+                },
+                Err(_) => {
+                    println!("error")
+                } 
             }
         }
     }
